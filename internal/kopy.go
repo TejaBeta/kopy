bcb10c609054fbaea01e373bf11fa9338a83c430
Tejasvi T <tejasvi.thota@gmail.com>
2020-10-02 22:40:00 +0100

    Slight modification to log message

--- internal/kopy.go
+++ internal/kopy.go
@@ -59,7 +59,7 @@ func Kopy(kopyOptions *options.KopyOptions) {
 		log.Println(sourceResources, destFOpts)
 
 		if destFOpts.IsValidNS() {
-			log.Info("Namespace exists at destinationresource will be overwritten.")
+			log.Info("Namespace ", kopyOptions.Namespace, " exists at destination all resource will be overwritten.")
 		} else {
 			log.Info("No namespace ", kopyOptions.Namespace, " found in destination context.")
 			log.Info("Namespace and resources will be created in the destination context.")
