ce7c25dca062a1c289e0ef6807051ef3c61acefc
Tejasvi T <tejasvi.thota@gmail.com>
2020-10-10 20:25:50 +0100

    Addition of namespace test case

---
+++ pkg/koperator/manipulate_test.go
@@ -0,0 +1,44 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package koperator
+
+import (
+	"context"
+	"testing"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	testclient "k8s.io/client-go/kubernetes/fake"
+)
+
+func TestNamespaceManipulation(t *testing.T) {
+
+	clientset := testclient.NewSimpleClientset()
+	ns := &v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "unit-test-ns"}}
+
+	namespace, err := clientset.CoreV1().Namespaces().Create(context.TODO(), ns, metav1.CreateOptions{})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	namespace.ResourceVersion = "12345"
+
+	ManipulateResource(namespace)
+
+	if namespace.ResourceVersion != "" {
+		t.Errorf("Manipulation of Namespace is failing")
+	}
+
+}
